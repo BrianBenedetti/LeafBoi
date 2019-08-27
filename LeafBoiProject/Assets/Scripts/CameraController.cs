05c7824dfa943d0e0e83e8b97176b94bcaa58ece
KekMadao <saatwikrk@gmail.com>
2019-08-27 23:23:20 +0200

    Camera follows the player around, only along the x-axis though

---
+++ LeafBoiProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform _position;
+    private GameObject _player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = PlayerController.instance.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(_player.transform.position.x, transform.position.y, transform.position.z);
+    }
+}
