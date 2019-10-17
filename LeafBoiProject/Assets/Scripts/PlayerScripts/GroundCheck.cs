f667e54f5089fcc45df48ca55166bc0d5cbb4482
KekMadao <saatwikrk@gmail.com>
2019-10-17 23:47:16 +0200

    SO COLLISION DETECTION WORKS (But there are some places that cause issues, that we can fix by changing the terrain at places, but yeah it's good)

--- LeafBoiProject/Assets/Scripts/PlayerScripts/GroundCheck.cs
+++ LeafBoiProject/Assets/Scripts/PlayerScripts/GroundCheck.cs
@@ -9,12 +9,121 @@ public class GroundCheck : MonoBehaviour
     public PlayerController player;
 
     public ParticleSystem ps;
+    public float maxDist;
 
+    [SerializeField] protected float minAngle;
+    [SerializeField] protected float maxAngle;
+    [SerializeField] protected float dotResist;
+    [SerializeField] protected int cAngle;
+    [SerializeField] protected float yAxis;
+
+    public bool frontStop;
+    public bool backStop;
+    public bool rightStop;
+    public bool leftStop;
 
     // Update is called once per frame
     void FixedUpdate()
     {
         bool groundRay = Physics.Raycast(transform.position, Vector3.down, dToGround);
+
+        Ray rFront = new Ray(transform.position + new Vector3(0, yAxis, 0), Quaternion.AngleAxis(-cAngle, transform.forward) * transform.right);
+        Ray rBack = new Ray(transform.position + new Vector3(0, yAxis, 0), Quaternion.AngleAxis(cAngle, transform.forward) * -transform.right);
+        Ray rRight = new Ray(transform.position + new Vector3(0, yAxis, 0), Quaternion.AngleAxis(-cAngle, transform.right) * -transform.forward);
+        Ray rLeft = new Ray(transform.position + new Vector3(0, yAxis, 0), Quaternion.AngleAxis(-cAngle, -transform.right) * transform.forward);
+
+        RaycastHit rFrontHit = new RaycastHit();
+        RaycastHit rBackHit = new RaycastHit();
+        RaycastHit rRightHit = new RaycastHit();
+        RaycastHit rLeftHit = new RaycastHit();
+
+        Debug.DrawRay(rFront.origin, player._moveDir * maxDist, Color.red);
+
+        if (Physics.Raycast(rFront, out rFrontHit, maxDist))
+        {
+            Debug.DrawRay(rFront.origin, rFrontHit.point - rFront.origin, Color.red);
+            float angle = Vector3.Angle(transform.forward, rFrontHit.normal);
+            float dot = Vector3.Dot(player._moveDir, rFront.direction);
+
+            if ((angle < minAngle || angle > maxAngle) && dot > dotResist)
+            {
+                frontStop = true;
+                print("Issues at the front");
+            }
+            else
+            {
+                frontStop = false;
+            }
+        }
+        else {
+            frontStop = false;
+        }
+
+        if (Physics.Raycast(rBack, out rBackHit, maxDist))
+        {
+            Debug.DrawRay(rBack.origin, rBackHit.point - rBack.origin, Color.red);
+            float angle = Vector3.Angle(transform.forward, rBackHit.normal);
+            float dot = Vector3.Dot(player._moveDir, rBack.direction);
+
+            if ((angle < minAngle || angle > maxAngle) && dot > dotResist)
+            {
+                backStop = true;
+                print("Issues at the back");
+            }
+            else
+            {
+                backStop = false;
+            }
+        }
+        else {
+            backStop = false;
+        }
+
+        if (Physics.Raycast(rRight, out rRightHit, maxDist))
+        {
+            Debug.DrawRay(rRight.origin, rRightHit.point - rRight.origin, Color.red);
+            float angle = Vector3.Angle(transform.right, rRightHit.normal);
+            float dot = Vector3.Dot(player._moveDir, rRight.direction);
+
+            if ((angle < minAngle || angle > maxAngle) && dot > dotResist)
+            {
+                rightStop = true;
+                print("Issues on the right");
+            }
+            else
+            {
+                rightStop = false;
+            }
+        }
+        else {
+            rightStop = false;
+        }
+
+        if (Physics.Raycast(rLeft, out rLeftHit, maxDist))
+        {
+            Debug.DrawRay(rLeft.origin, rLeftHit.point - rLeft.origin, Color.red);
+            float angle = Vector3.Angle(transform.right, rLeftHit.normal);
+            float dot = Vector3.Dot(player._moveDir, rLeft.direction);
+
+            if ((angle < minAngle || angle > maxAngle) && dot > dotResist)
+            {
+                leftStop = true;
+                print("Issues on the left");
+            }
+            else
+            {
+                leftStop = false;
+            }
+        }
+        else {
+            leftStop = false;
+        }
+
+
+
+
+
+
 
         if (groundRay)
         {
