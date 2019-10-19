363e2062ff836e53202b491c2d8b92a6a6e86fd7
KekMadao <saatwikrk@gmail.com>
2019-10-19 12:20:21 +0200

    Set up for sounds and particle effects to be added to script

--- LeafBoiProject/Assets/Scripts/PlayerScripts/GroundCheck.cs
+++ LeafBoiProject/Assets/Scripts/PlayerScripts/GroundCheck.cs
@@ -22,6 +22,7 @@ public class GroundCheck : MonoBehaviour
     public bool rightStop;
     public bool leftStop;
     public LayerMask layerMask;
+    private bool landed = false;
 
     // Update is called once per frame
     void FixedUpdate()
@@ -122,8 +123,16 @@ public class GroundCheck : MonoBehaviour
 
 
 
-
-
+        if (groundRay && !landed)
+        {
+            //_INSERT LANDING NOISES HERE_
+            landed = true;
+            print("Landed");
+        }
+        else if(!groundRay)
+        {
+            landed = false;
+        }
 
 
         if (groundRay)
