7c180c5c5f74d73cc37d378523ea527bba30cdcc
KekMadao <saatwikrk@gmail.com>
2019-10-21 12:08:26 +0200

    Added Script for event changes

---
+++ LeafBoiProject/Assets/Scripts/WorldScripts/WorldStateChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldStateChange : MonoBehaviour
+{
+    [SerializeField] protected GameManager gm;
+    [SerializeField] protected int state;
+
+    public void stateChange() {
+        gm.UpdateState(state);
+        //Change Shader to increase level of the blight in level
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
