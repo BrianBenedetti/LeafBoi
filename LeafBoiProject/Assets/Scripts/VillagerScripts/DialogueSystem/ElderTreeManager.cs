94e27a8058bf79b88cc127eb98efbbf43b2b8f52
KekMadao <saatwikrk@gmail.com>
2019-11-13 12:08:28 +0200

    Sage tree only gets animated once the player has collected the 2nd sap

---
+++ LeafBoiProject/Assets/Scripts/VillagerScripts/DialogueSystem/ElderTreeManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElderTreeManager : MonoBehaviour
+{
+    [SerializeField] protected GameManager _gm;
+    [SerializeField] protected Animator _anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_gm.GameState > 2)
+        {
+            _anim.enabled = true;
+        }
+    }
+}
